ac5f6c848612cff393d0313a9190ea6eb84f5cb9
mohsin100145 <mohsin.cse.45@gmail.com>
2018-03-12 17:46:13 +0600

    aft_ubu_conf

--- database/migrations/2018_02_05_145741_create_carriers_table.php
+++ database/migrations/2018_02_05_145741_create_carriers_table.php
@@ -14,10 +14,25 @@ class CreateCarriersTable extends Migration
     {
         Schema::create('carriers', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('carrier_id');
-            $table->string('registration_string');
-            $table->text('account_entry');
-            $table->text('dialplan_entry');
+            $table->string('carrierid');
+            $table->string('register');
+            $table->string('disallow');
+            $table->string('type');
+            $table->string('dtmfmode');
+            $table->string('context');
+            $table->string('qualify');
+            $table->string('insecure');
+            $table->string('nat');
+            $table->string('host');
+            $table->string('username');
+            $table->string('secret');
+            $table->string('gsmallow');
+            $table->string('alawallow');
+            $table->string('ulawallow');
+            $table->string('AGIexten');
+            $table->string('SETexten');
+            $table->string('Dialexten');
+            $table->string('Hangupexten');
             $table->integer('created_by')->unsigned()->nullable();
             $table->integer('updated_by')->unsigned()->nullable();
             $table->integer('deleted_by')->unsigned()->nullable();
